75aeec98dfc40aaab049dbc8f08ce884f8515189
putteneersjoris <putteneersjoris@gmail.com>
2023-08-07 16:19:36 +0200

    new update2

--- src/data.js
+++ src/data.js
@@ -1,43 +1,209 @@
-var folder_1 = {
-'folder_name':'folder 1',
-'images': ['./data/folder 1/header - Copy.png', './data/folder 1/header.png', './data/folder 1/jorisputteneers_data_driven_hyperreal_bismuth_architecture_houd_b5c6a655-7d58-41ed-928a-12398fe71c1f - Copy.png', './data/folder 1/jorisputteneers_data_driven_hyperreal_bismuth_architecture_houd_b5c6a655-7d58-41ed-928a-12398fe71c1f.png', './data/folder 1/jorisputteneers_fully_black_highly_intricate_obfuscated_archite_c27a180a-bb97-458c-a7e0-29fc86432d8e - Copy.png', './data/folder 1/jorisputteneers_fully_black_highly_intricate_obfuscated_archite_c27a180a-bb97-458c-a7e0-29fc86432d8e.png', './data/folder 1/jorisputteneers_fully_black_highly_intricate_obfuscated_archite_d93a9509-8bdd-4930-a1c2-5dab2831aa4e - Copy.png', './data/folder 1/jorisputteneers_fully_black_highly_intricate_obfuscated_archite_d93a9509-8bdd-4930-a1c2-5dab2831aa4e.png', './data/folder 1/jorisputteneers_hyperfocused_obfuscated_architecture_by_houdini_0e40cb12-2a52-48c1-95ca-8ca3c1e316a4 - Copy.png', './data/folder 1/jorisputteneers_hyperfocused_obfuscated_architecture_by_houdini_0e40cb12-2a52-48c1-95ca-8ca3c1e316a4.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_0967b52a-5fcd-4a57-901f-c32cb34deff7 - Copy.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_0967b52a-5fcd-4a57-901f-c32cb34deff7.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_137adfb0-b394-4839-a1c9-31bb6f7f10d5.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_14d94363-60b2-4023-9780-2ae4fc3864d6 - Copy.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_14d94363-60b2-4023-9780-2ae4fc3864d6.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_5d8567dc-55f7-4895-ad87-426a57fa2f53 - Copy.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_5d8567dc-55f7-4895-ad87-426a57fa2f53.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_82b01565-85bb-4722-86b7-b9239658d199 - Copy.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_82b01565-85bb-4722-86b7-b9239658d199.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_83c19e1f-a5b0-44d0-ae1b-f9517c8bc091 - Copy.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_83c19e1f-a5b0-44d0-ae1b-f9517c8bc091.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_9c22f35d-e599-4287-b92e-eefed213d4a6 - Copy.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_9c22f35d-e599-4287-b92e-eefed213d4a6.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_a4343b26-bdf5-43d9-bf5d-a77f67e898a3.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_c6a71b3e-cc95-43f1-9862-ca8d9b4ed1be.png', './data/folder 1/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_d98cfc3c-7db1-496e-a5c1-02f80d5c8a6d.png', './data/folder 1/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_c6bc4bfe-57d9-453d-ad03-2b12c1d44a9a - Copy.png', './data/folder 1/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_c6bc4bfe-57d9-453d-ad03-2b12c1d44a9a.png', './data/folder 1/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_d75b3d22-3439-42cb-a5db-b6adccd612e2 - Copy.png', './data/folder 1/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_d75b3d22-3439-42cb-a5db-b6adccd612e2.png', ],
-'text':'./data/folder 1/poem3.txt',
-'header_index': 1
-}
-
-var folder_2 = {
-'folder_name':'folder 2',
-'images': ['./data/folder 2/header.png', './data/folder 2/q (1).png', './data/folder 2/q (2).png', './data/folder 2/q (4).png', './data/folder 2/q (5).png', './data/folder 2/q (6).png', ],
-'text':'./data/folder 2/poem3.txt',
-'header_index': 0
-}
-
-var folder_3 = {
-'folder_name':'folder 3',
-'images': ['./data/folder 3/header.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_0967b52a-5fcd-4a57-901f-c32cb34deff7.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_137adfb0-b394-4839-a1c9-31bb6f7f10d5.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_82b01565-85bb-4722-86b7-b9239658d199.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_a4343b26-bdf5-43d9-bf5d-a77f67e898a3.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_c6a71b3e-cc95-43f1-9862-ca8d9b4ed1be.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_d313ceb1-5964-44cd-b57c-a80e53d66735.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_d98cfc3c-7db1-496e-a5c1-02f80d5c8a6d.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_e0973d26-cd4e-4896-933d-3abf01c1cec8.png', './data/folder 3/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_e21cc43b-3b13-4951-ac0f-4fc2cd764785.png', './data/folder 3/jorisputteneers_hyperreal_hyperfocussed_obfuscated_architecture_120ad57a-4522-4513-8a6c-5016a8a8904a.png', './data/folder 3/jorisputteneers_hyperreal_hyperfocussed_obfuscated_architecture_3c79ffdb-3150-494c-b81c-ee2cf7a7cb72.png', './data/folder 3/jorisputteneers_hyperreal_hyperfocussed_obfuscated_architecture_a65f069c-1925-4310-9042-c815a4b5c94f - Copy (2).png', ],
-'text':'./data/folder 3/poem3.txt',
-'header_index': 0
-}
-
-var folder_4 = {
-'folder_name':'folder 4',
-'images': ['./data/folder 4/header.png', './data/folder 4/hg.png', './data/folder 4/q (1).png', './data/folder 4/q (2).png', './data/folder 4/q (4).png', './data/folder 4/q (5).png', ],
-'text':'./data/folder 4/poem1.txt',
-'header_index': 0
-}
-
-var folder_5 = {
-'folder_name':'folder 5',
-'images': ['./data/folder 5/header.png', './data/folder 5/jorisputteneers_data_driven_hyperreal_bismuth_architecture_houd_b5c6a655-7d58-41ed-928a-12398fe71c1f.png', './data/folder 5/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_d313ceb1-5964-44cd-b57c-a80e53d66735.png', './data/folder 5/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_e21cc43b-3b13-4951-ac0f-4fc2cd764785.png', './data/folder 5/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_d75b3d22-3439-42cb-a5db-b6adccd612e2.png', ],
-'text':'./data/folder 5/poem3.txt',
-'header_index': 0
-}
-
-var folder_6 = {
-'folder_name':'folder 6',
-'images': ['./data/folder 6/ad (3).png', './data/folder 6/ad (4).png', './data/folder 6/ad (5).png', './data/folder 6/ad (6).png', './data/folder 6/ad (7).png', './data/folder 6/ad (8).png', './data/folder 6/fggg (1).png', './data/folder 6/fggg (2).png', './data/folder 6/fggg (3).png', './data/folder 6/fggg (4).png', './data/folder 6/fggg (5).png', './data/folder 6/fggg (6).png', './data/folder 6/fggg (7).png', './data/folder 6/header.png', './data/folder 6/jorisputteneers_data_driven_hyperreal_bismuth_architecture_houd_b5c6a655-7d58-41ed-928a-12398fe71c1f.png', './data/folder 6/jorisputteneers_fully_black_highly_intricate_obfuscated_archite_c27a180a-bb97-458c-a7e0-29fc86432d8e.png', './data/folder 6/jorisputteneers_fully_black_highly_intricate_obfuscated_archite_d93a9509-8bdd-4930-a1c2-5dab2831aa4e.png', './data/folder 6/jorisputteneers_hyperfocused_obfuscated_architecture_by_houdini_0e40cb12-2a52-48c1-95ca-8ca3c1e316a4.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_0967b52a-5fcd-4a57-901f-c32cb34deff7.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_137adfb0-b394-4839-a1c9-31bb6f7f10d5.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_14d94363-60b2-4023-9780-2ae4fc3864d6.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_5d8567dc-55f7-4895-ad87-426a57fa2f53.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_82b01565-85bb-4722-86b7-b9239658d199.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_83c19e1f-a5b0-44d0-ae1b-f9517c8bc091.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_9c22f35d-e599-4287-b92e-eefed213d4a6.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_a4343b26-bdf5-43d9-bf5d-a77f67e898a3.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_c6a71b3e-cc95-43f1-9862-ca8d9b4ed1be.png', './data/folder 6/jorisputteneers_hyperreal_highly_detailed_obfuscated_architectu_d98cfc3c-7db1-496e-a5c1-02f80d5c8a6d.png', './data/folder 6/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_c6bc4bfe-57d9-453d-ad03-2b12c1d44a9a.png', './data/folder 6/jorisputteneers__fully_black_highly_intricate_obfuscated_archit_d75b3d22-3439-42cb-a5db-b6adccd612e2.png', ],
-'text':'./data/folder 6/poem3.txt',
-'header_index': 13
-}
-
-var objects = ['folder_1', 'folder_2', 'folder_3', 'folder_4', 'folder_5', 'folder_6']
+var about = {
+    "eveline": [
+        {
+            "images": [
+                "./content/about/eveline/header.png",
+                "./content/about/eveline/b.png",
+                "./content/about/eveline/a.png",
+                "./content/about/eveline/c.jpg"
+            ]
+        },
+        {
+            "text": "./content/about/eveline/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/about/eveline/header.png"
+        }
+    ],
+    "frank": [
+        {
+            "images": [
+                "./content/about/frank/s.png",
+                "./content/about/frank/header.png"
+            ]
+        },
+        {
+            "text": "./content/about/frank/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/about/frank/header.png"
+        }
+    ]
+} 
+var articles = {
+    "article_drawing": [
+        {
+            "images": [
+                "./content/articles/article_drawing/DAA.png",
+                "./content/articles/article_drawing/b.png",
+                "./content/articles/article_drawing/a.png",
+                "./content/articles/article_drawing/sssss.png",
+                "./content/articles/article_drawing/xxx.png",
+                "./content/articles/article_drawing/IMG_9752-768x576.jpg",
+                "./content/articles/article_drawing/IMG_9711-768x576.jpg",
+                "./content/articles/article_drawing/cloud.jpg",
+                "./content/articles/article_drawing/matrix-1.jpg",
+                "./content/articles/article_drawing/cloud-2-1.jpg",
+                "./content/articles/article_drawing/movement-1.jpg",
+                "./content/articles/article_drawing/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/articles/article_drawing/cropped-banner-miaw-1-768x401.jpg",
+                "./content/articles/article_drawing/Recorded-personal-movements_Q320.jpg",
+                "./content/articles/article_drawing/IMG_5627-reduced-768x1024.jpg",
+                "./content/articles/article_drawing/Thierry-Lagrange.jpg",
+                "./content/articles/article_drawing/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/articles/article_drawing/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/articles/article_drawing/header.jpg"
+        }
+    ],
+    "drawing": [
+        {
+            "images": [
+                "./content/articles/drawing/DAA.png",
+                "./content/articles/drawing/c.png",
+                "./content/articles/drawing/b.png",
+                "./content/articles/drawing/a.png",
+                "./content/articles/drawing/sssss.png",
+                "./content/articles/drawing/IMG_9752-768x576.jpg",
+                "./content/articles/drawing/cloud.jpg",
+                "./content/articles/drawing/matrix-1.jpg",
+                "./content/articles/drawing/cloud-2-1.jpg",
+                "./content/articles/drawing/69-714x1024.jpg",
+                "./content/articles/drawing/movement-1.jpg",
+                "./content/articles/drawing/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/articles/drawing/cropped-banner-miaw-1-768x401.jpg",
+                "./content/articles/drawing/Recorded-personal-movements_Q320.jpg",
+                "./content/articles/drawing/IMG_5627-reduced-768x1024.jpg",
+                "./content/articles/drawing/Thierry-Lagrange.jpg",
+                "./content/articles/drawing/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/articles/drawing/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/articles/drawing/header.jpg"
+        }
+    ]
+} 
+var projects = {
+    "brick": [
+        {
+            "images": [
+                "./content/projects/brick/DAA.png",
+                "./content/projects/brick/c.png",
+                "./content/projects/brick/b.png",
+                "./content/projects/brick/a.png",
+                "./content/projects/brick/sssss.png",
+                "./content/projects/brick/IMG_9752-768x576.jpg",
+                "./content/projects/brick/IMG_9711-768x576.jpg",
+                "./content/projects/brick/cloud.jpg",
+                "./content/projects/brick/cloud-2-1.jpg",
+                "./content/projects/brick/69-714x1024.jpg",
+                "./content/projects/brick/movement-1.jpg",
+                "./content/projects/brick/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/projects/brick/cropped-banner-miaw-1-768x401.jpg",
+                "./content/projects/brick/Recorded-personal-movements_Q320.jpg",
+                "./content/projects/brick/Thierry-Lagrange.jpg",
+                "./content/projects/brick/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/projects/brick/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/projects/brick/header.jpg"
+        }
+    ],
+    "foundation": [
+        {
+            "images": [
+                "./content/projects/foundation/DAA.png",
+                "./content/projects/foundation/c.png",
+                "./content/projects/foundation/b.png",
+                "./content/projects/foundation/a.png",
+                "./content/projects/foundation/sssss.png",
+                "./content/projects/foundation/IMG_9752-768x576.jpg",
+                "./content/projects/foundation/IMG_9711-768x576.jpg",
+                "./content/projects/foundation/cloud.jpg",
+                "./content/projects/foundation/matrix-1.jpg",
+                "./content/projects/foundation/cloud-2-1.jpg",
+                "./content/projects/foundation/69-714x1024.jpg",
+                "./content/projects/foundation/movement-1.jpg",
+                "./content/projects/foundation/cropped-banner-miaw-1-768x401.jpg",
+                "./content/projects/foundation/Recorded-personal-movements_Q320.jpg",
+                "./content/projects/foundation/IMG_5627-reduced-768x1024.jpg",
+                "./content/projects/foundation/Thierry-Lagrange.jpg",
+                "./content/projects/foundation/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/projects/foundation/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/projects/foundation/header.jpg"
+        }
+    ]
+} 
+var workshops = {
+    "ciry": [
+        {
+            "images": [
+                "./content/workshops/ciry/DAA.png",
+                "./content/workshops/ciry/c.png",
+                "./content/workshops/ciry/header.png",
+                "./content/workshops/ciry/b.png",
+                "./content/workshops/ciry/sssss.png",
+                "./content/workshops/ciry/IMG_9752-768x576.jpg",
+                "./content/workshops/ciry/IMG_9711-768x576.jpg",
+                "./content/workshops/ciry/matrix-1.jpg",
+                "./content/workshops/ciry/cloud-2-1.jpg",
+                "./content/workshops/ciry/69-714x1024.jpg",
+                "./content/workshops/ciry/movement-1.jpg",
+                "./content/workshops/ciry/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/workshops/ciry/cropped-banner-miaw-1-768x401.jpg",
+                "./content/workshops/ciry/Recorded-personal-movements_Q320.jpg",
+                "./content/workshops/ciry/IMG_5627-reduced-768x1024.jpg",
+                "./content/workshops/ciry/Thierry-Lagrange.jpg"
+            ]
+        },
+        {
+            "text": "./content/workshops/ciry/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/workshops/ciry/header.png"
+        }
+    ],
+    "thoughts on sketching while walking the dog": [
+        {
+            "images": [
+                "./content/workshops/thoughts on sketching while walking the dog/DAA.png",
+                "./content/workshops/thoughts on sketching while walking the dog/c.png",
+                "./content/workshops/thoughts on sketching while walking the dog/b.png",
+                "./content/workshops/thoughts on sketching while walking the dog/a.png",
+                "./content/workshops/thoughts on sketching while walking the dog/sssss.png",
+                "./content/workshops/thoughts on sketching while walking the dog/IMG_9752-768x576.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/IMG_9711-768x576.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/matrix-1.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/cloud-2-1.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/69-714x1024.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/movement-1.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/cropped-banner-miaw-1-768x401.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/Recorded-personal-movements_Q320.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/IMG_5627-reduced-768x1024.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/Thierry-Lagrange.jpg"
+            ]
+        },
+        {
+            "text": "./content/workshops/thoughts on sketching while walking the dog/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/workshops/thoughts on sketching while walking the dog/header.jpeg"
+        }
+    ]
+} 
